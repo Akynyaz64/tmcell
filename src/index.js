96d45215b1364958bdebf3469a93041318e36c3c
Akynyaz64 <mr.akynyaz29@gmail.com>
2023-03-04 15:51:01 +0500

    init repo

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+import "jquery/dist/jquery.min.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
