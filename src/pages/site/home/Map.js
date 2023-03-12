b308576cedd79204b32f72d588e00f00cf8d7470
Akynyaz64 <mr.akynyaz29@gmail.com>
2023-03-12 15:14:51 +0500

    add map js

---
+++ src/pages/site/home/Map.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { ReactComponent as MapSvg } from "../../../assets/icons/map.svg";
+
+const Map = () => {
+    return (
+        <div className="map">
+            <MapSvg></MapSvg>
+        </div>
+    );
+};
+
+export default Map;
